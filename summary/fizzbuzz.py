78bdd270e9f7c6fc634ac9959e054d2926ca2db0
GeraldHeng93 <109711716+GeraldHeng93@users.noreply.github.com>
2022-08-18 16:39:17 +0800

    Update fizzbuzz.py

--- summary/fizzbuzz.py
+++ summary/fizzbuzz.py
@@ -1,9 +1,14 @@
-for i in range(1, 16):
-    if i % 3 == 0 and i % 5 == 0:
-        print ('FizzBuzz')
-    elif i % 3 == 0:
-        print ('Fizz')
-    elif i % 5 == 0:
-        print ('Buzz')
-    else:
-        print (str(i))
+#! /usr/bin/python
+
+num = 10
+for i in range(0, num+1):
+	if i == num:
+		print("TRADITION")
+	elif i % 15 == 0:
+		print("fizz buzz")
+	elif i % 5 == 0:
+		print("buzz")
+	elif i % 3 == 0:
+		print("fizz")
+	else:
+		print(i)
